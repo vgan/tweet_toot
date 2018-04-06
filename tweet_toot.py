f166e6673d62e9dfa1201b591f5d91beaea0f3e8
Steve <vgan@users.noreply.github.com>
2018-04-06 13:11:28 -0400

    Update tweet_toot.py
    Changed name, added char length check.

--- tweet_toot.py
+++ tweet_toot.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import tweepy
+import tweepy, sys
 from keys import *
+from random import choice
 from mastodon import Mastodon
-import sys
 
 mastodon = Mastodon(
         api_base_url='https://mastodon.social',
-        client_id = '/home/vgan/tweet_toot/tweet_toot_clientcred.txt',
-        access_token = '/home/vgan/tweet_toot/tweet_toot_usercred.txt'
+        client_id = '/home/vgan/twoot/twoot_clientcred.txt',
+        access_token = '/home/vgan/twoot/twoot_usercred.txt'
 )
 auth = tweepy.OAuthHandler(twitter_consumer_key, twitter_consumer_secret)
 auth.set_access_token(twitter_token_key, twitter_token_secret)
 api = tweepy.API(auth)
 
 text = sys.argv[1]
+length = len(text)
 
-try:
-        status = api.update_status(status=text)
-except:
-        print "tweet failed"
-
-try:
-        tooterino = mastodon.status_post(text, sensitive=False)
-except:
-        print "toot failed"
+if length <= 279:
+        print str(length) + " characters..."
+        if length == 69:
+                print "nice."
+        try:
+                status = api.update_status(status=text)
+                print "tweeted."
+        except:
+                print "twete failed :("
+        try:
+                tooterino = mastodon.status_post(text, sensitive=False)
+                print "tooted."
+        except:
+                print "awoo failed :(("
+else:
+        thismany = length - 280
+        print str(length) + " is " + str(thismany) + " too many characters for twitter."
